310bd512debf8e8e0496a5a72d156a87f34a9b21
tallner <ctallner@hotmail.com>
2022-12-05 09:44:25 +0100

    Initial commit

---
+++ src/test/java/com/cte/SetupTest.java
@@ -0,0 +1,14 @@
+package com.cte;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SetupTest {
+    @Test
+    public void checkIfChecks(){
+
+        assertEquals("expected","expected");
+
+    }
+}
+
